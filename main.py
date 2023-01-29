83d9b69f82f829193945e4b470e93a4fa2863b09
SameriteRL <raymondchen733@gmail.com>
2023-01-29 03:37:47 -0500

    Updated main.py and added globals.py
    Organized certain variables previously in main.py into a globals.py file, changed the token to read from a text file instead. Added a custom startup message displayed in the console.

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
+import platform
 import discord
 from discord.ext import commands
-from globals import TOKEN
+from globals import *
 
-bot = commands.Bot(command_prefix="?", owner_id=230003732836909056, intents=discord.Intents.all())
+TOKEN = open("TOKEN.txt").read()
+
+intents = discord.Intents.default()
+intents.members = True
+intents.message_content = True
+bot = commands.Bot(command_prefix=COMMAND_PREFIX, owner_ids=OWNER_IDS, intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user.name}#{bot.user.discriminator}\n'\
+    f'Python version {platform.python_version()}\n'\
+    f'Discord.py version {discord.__version__}')
 
 def main():
     bot.run(TOKEN)
