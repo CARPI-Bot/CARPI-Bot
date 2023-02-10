ef4c531ca972919211dcd0ac451e05f69aa1f4bf
Justin Isaac <kbulkiski@gmail.com>
2023-02-10 17:45:59 -0500

    Added Stinky command
    Pings a random member. Hard coded and should be able to get max number of members soon.

--- cogs/fun.py
+++ cogs/fun.py
@@ -6,9 +6,16 @@ class Fun(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(description='Pings sender', alias=['hi'])
+    @commands.command(description='Pings sender', aliases=["hi","hey"])
     async def hello(self, ctx):
         await ctx.send(f"Hello {ctx.author.mention}!")
+
+    
+    @commands.command(description='Pings random member and calls them stinky')
+    async def stinky(self, ctx):
+        #Get random position of memberlist
+        m = random.randint(0, 9)
+        await ctx.send(f"{ctx.guild.members[m].mention} is stinky!")
 
 async def setup(bot):
     await bot.add_cog(Fun(bot))
