8a5f4c6b239268c789be0141f2d7a367075b6285
Justin Isaac <kbulkiski@gmail.com>
2023-02-10 17:08:02 -0500

    Created cog Fun.py
    experimental/random commands

---
+++ cogs/fun.py
@@ -0,0 +1,15 @@
+from discord.ext import commands
+from globals import *
+import random
+
+class Fun(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(description='Pings sender')
+    @commands.command(alias=["hi"])
+    async def hello(self, ctx):
+        await ctx.send("Hello {ctx.author.mention}!")
+
+async def setup(bot):
+    await bot.add_cog(Calculator(bot))
