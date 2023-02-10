c2efaee828561dc6572977d99395528f98ed844a
SameriteRL <raymondchen733@gmail.com>
2023-02-10 17:35:46 -0500

    Hid Moderator cog and updated intents

--- main.py
+++ main.py
@@ -4,13 +4,10 @@ import os
 import discord
 from discord.ext import commands
 from globals import *
-from cogs import calculator, modtools, timedevents
 
 TOKEN = open("TOKEN.txt").read()
 
-intents = discord.Intents.default()
-intents.members = True
-intents.message_content = True
+intents = discord.Intents.all()
 bot = commands.Bot(command_prefix=COMMAND_PREFIX, owner_ids=OWNER_IDS, intents=intents)
 
 async def loadCogs():
@@ -20,9 +17,6 @@ async def loadCogs():
 
 @bot.event
 async def on_ready():
-    await bot.add_cog(calculator.Calculator(bot))
-    await bot.add_cog(modtools.Moderator(bot))
-    await bot.add_cog(timedevents.TimedEvents(bot))
     print(f"Logged in as {bot.user.name}#{bot.user.discriminator}")
     print(f"Python version {platform.python_version()}")
     print(f"Discord.py version {discord.__version__}")
