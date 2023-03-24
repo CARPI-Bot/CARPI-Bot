0e59162ef8c224e0d66e8b7de46dc4f91df2b1ee
SameriteRL <raymondchen733@gmail.com>
2023-03-24 16:52:00 -0400

    Moderator and Miscellaneous commands are now hybrid

--- cogs/miscellaneous.py
+++ cogs/miscellaneous.py
@@ -9,7 +9,7 @@ class Miscellaneous(commands.Cog):
         self.bot = bot
 
     ### PING ###
-    @commands.command(description="Gets the latency of the server")
+    @commands.hybrid_command(description="Gets the latency of the server")
     async def ping(self, ctx):
         # Retrieves the server latency in milliseconds
         embedVar = discord.Embed(title="Pong!", description=f"Your message was recieved in {round(self.bot.latency * 1000)}ms.", color=0x00C500, timestamp=datetime.datetime.now())
@@ -23,7 +23,7 @@ class Miscellaneous(commands.Cog):
             await ctx.send("Something went wrong.")
     
     ### AVATAR ###
-    @commands.command(description="Gets the avatar of any user")
+    @commands.hybrid_command(description="Gets the avatar of any user")
     async def avatar(self, ctx, member:discord.Member=None):
         if (member == None):
             await ctx.send(ctx.author.display_avatar)
@@ -39,7 +39,7 @@ class Miscellaneous(commands.Cog):
             await ctx.send("Invalid member.")
 
     ### COINFLIP ###
-    @commands.command(description="Flips a coin. Can either be heads or tails.", aliases=["flip", "coin"])
+    @commands.hybrid_command(description="Flips a coin. Can either be heads or tails.", aliases=["flip", "coin"])
     async def coinflip(self, ctx):
         # Calls a random float between 0 and 0.99 inclusive. Returns heads if 0 - 0.48 and tails if 0.49 - 0.99
         result = "Heads!" if random.randint(0, 1) == 0 else "Tails!"
@@ -54,7 +54,7 @@ class Miscellaneous(commands.Cog):
             await ctx.send("Something went wrong.")
 
     ### REPO ###
-    @commands.command(description="Returns a message that can be interacted with (clicked on) bringing a user to our GitHub Repo.")
+    @commands.hybrid_command(description="Returns a message that can be interacted with (clicked on) bringing a user to our GitHub Repo.")
     async def repo(self, ctx):
         embedVar = discord.Embed(title=f"Click Here to Redirect to the {ctx.guild.get_member(1067560443444478034).name} Repository", url='https://github.com/Zen1124/tsdb', color=0x0099FF, timestamp=datetime.datetime.now())
         embedVar.set_footer(text=f"\u200bRepository link requested by {ctx.author.nick}")
@@ -67,7 +67,7 @@ class Miscellaneous(commands.Cog):
             await ctx.send("Something went wrong.")
     
     ### SECRET MESSAGE ###
-    @commands.command(description="Secretly message someone")
+    @commands.hybrid_command(description="Secretly message someone")
     async def secretmessage(self, ctx, members: commands.Greedy[discord.Member], *, msg="Hey!"):
         for x in members:
             if not x.dm_channel == None:
