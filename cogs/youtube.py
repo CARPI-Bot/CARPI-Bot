1ed53cca28fcae61d05bbdd5eebe492a3b2c5352
liug88 <gavinliu368@gmail.com>
2023-04-21 14:48:05 -0400

    Implemented Youtube Search
    Added a command that SHOULD be able to return a youtube video given a search query

--- cogs/youtube.py
+++ cogs/youtube.py
@@ -1,14 +1,20 @@
+import discord
+import googlesearch
 from discord.ext import commands
 from globals import *
-import discord
-import urllib.parse, urllib.request, re
+
 
 class Youtube(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    ### YOUTUBE SEARCHER ### 
-
+    @commands.command(name='ytsearch')
+    async def youtube_search(self, ctx, *, query):
+        search_results = list(googlesearch.search(f'{query} site:youtube.com/watch', num_results=1))
+        if search_results:
+            await ctx.send(search_results[0])
+        else:
+            await ctx.send(f"No search results found for '{query}' on YouTube.")
 
 async def setup(bot):
     await bot.add_cog(Youtube(bot))
