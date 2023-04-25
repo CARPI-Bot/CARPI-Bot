83d17bb521d7081e318adc40de79a7df2a3b595d
SameriteRL <raymondchen733@gmail.com>
2023-04-25 16:45:37 -0400

    Bot client is now a class separate from main file

--- main.py
+++ main.py
@@ -1,40 +1,10 @@
 import asyncio
-import discord
-import googlesearch
-import os
-from discord.ext import commands
+from src.bot import Bot
 from globals import *
 
-
-TOKEN = open("TOKEN.txt").read().strip()
-
-intents = discord.Intents.all()
-bot = commands.Bot(command_prefix=COMMAND_PREFIX, owner_ids=OWNER_IDS, intents=intents)
-
-async def loadCogs(bot:commands.Bot):
-    for file_name in os.listdir('./cogs'):
-        if file_name.endswith('.py'):
-            await bot.load_extension(f"cogs.{file_name[:-3]}")
-    for file_name in os.listdir('./cogs/calendar'):
-        if file_name.endswith('.py'):
-            await bot.load_extension(f"cogs.{file_name[:-3]}")
-
-@bot.event
-async def on_ready():
-    num_synced = len(await bot.tree.sync())
-    print("==================================")
-    print(">>" + f"Logged in as {bot.user.name}#{bot.user.discriminator}".center(30) + "<<")
-    print(">>" + f"Discord.py version {discord.__version__}".center(30) + "<<")
-    print(f"{num_synced} command(s) synced!".center(34))
-    print("==================================\n")
-    print(f"Currently deployed in {len(bot.guilds)} guild(s):")
-    for guild in bot.guilds:
-        print(" - " + guild.name)
-    print("\n==================================")
-
 async def main():
-    await loadCogs(bot)
-    await bot.start(TOKEN)
-    
+    bot = Bot(COMMAND_PREFIX, discord.Intents.all())
+    await bot.start(bot.get_token())
+
 if __name__ == "__main__":
     asyncio.run(main())
---
+++ src/bot.py
@@ -0,0 +1,38 @@
+import os
+import discord
+from discord.ext import commands
+from globals import *
+
+async def load_extensions(path:str, bot:commands.Bot):
+    for file_name in os.listdir(path):
+        new_path = os.path.join(path, file_name)
+        if os.path.isdir(new_path):
+            await load_extensions(os.path.join(new_path), bot)
+        else:
+            if file_name.endswith(".py"):
+                try: await bot.load_extension(f"cogs.{file_name[:-3]}")
+                except: pass
+
+class Bot(commands.Bot):
+
+    def __init__(self, prefix:str, intents:discord.Intents):
+        super().__init__(command_prefix=prefix, intents=intents)
+        self.token = open("TOKEN.txt").read().strip()
+
+    async def setup_hook(self):
+        await load_extensions("./cogs", self)
+    
+    async def on_ready(self):
+        num_synced = len(await self.tree.sync())
+        print("==================================")
+        print(">>" + f"Logged in as {self.user.name}#{self.user.discriminator}".center(30) + "<<")
+        print(">>" + f"Discord.py version {discord.__version__}".center(30) + "<<")
+        print(f"{num_synced} command(s) synced!".center(34))
+        print("==================================\n")
+        print(f"Currently deployed in {len(self.guilds)} guild(s):")
+        for guild in self.guilds:
+            print(" - " + guild.name)
+        print("\n==================================")
+    
+    async def get_token(self):
+        return self.token
