0da7450a241417f9beb3642fb5e4218b09d967b0
liug88 <gavinliu368@gmail.com>
2023-04-25 12:33:14 -0400

    Updated Academic Calendar
    Recommitted code of the academic calendar that we have so far, we can work on finishing this later.

--- academic_cal.py
+++ academic_cal.py
@@ -49,44 +49,44 @@ if (datetime.now() == day):
     print(datetime.now())
 
 
-# class AcadCal(commands.Cog):
-#     def __init__(self, bot):
-#         self.bot = bot
-#         self.url = "https://info.rpi.edu/registrar/academic-calendar"
-#         self.events = []
+class AcadCal(commands.Cog):
+     def __init__(self, bot):
+        self.bot = bot
+        self.url = "https://info.rpi.edu/registrar/academic-calendar"
+        self.events = []
 
-#     def get_dates(self):
-#         URL = "https://info.rpi.edu/registrar/academic-calendar"
-#         header = {
-#             'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36',
-#         }
+     def get_dates(self):
+         URL = "https://info.rpi.edu/registrar/academic-calendar"
+         header = {
+             'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36',
+         }
 
-#         ## getting the HTML content from the webpage
-#         r = requests.get(self.URL, headers = header)
-#         soup = BeautifulSoup(r.content, 'html5lib')
+         ## getting the HTML content from the webpage
+         r = requests.get(self.URL, headers = header)
+         soup = BeautifulSoup(r.content, 'html5lib')
 
-#         ## finding the div container storing all the calender dates
-#         academicCal = soup.find('div', attrs = {'id' : "academicCalendar"})
+         ## finding the div container storing all the calender dates
+         academicCal = soup.find('div', attrs = {'id' : "academicCalendar"})
 
-#         ## storing calendar dates into a list of dictionaries
-#         dates = []
+         ## storing calendar dates into a list of dictionaries
+         dates = []
 
-#         for month in academicCal.find_all('table'):
-#             rows = month.find_all('tr')  
-#             month = rows[0].text  
+         for month in academicCal.find_all('table'):
+             rows = month.find_all('tr')  
+             month = rows[0].text  
 
-#             for d in range(len(rows)-2):
-#                 date = {}
-#                 date['month'] = month
-#                 date['date'] = rows[d+1].find_all('td')[0].text
-#                 date['event'] = rows[d+1].find_all('td')[1].text
-#                 date['url'] = rows[d+1].a['href']
-#                 dates.append(date)
+             for d in range(len(rows)-2):
+                 date = {}
+                 date['month'] = month
+                 date['date'] = rows[d+1].find_all('td')[0].text
+                 date['event'] = rows[d+1].find_all('td')[1].text
+                 date['url'] = rows[d+1].a['href']
+                 dates.append(date)
 
-#         for day in dates:
-#             print(day['date'], day['event'], sep="\n")
-#             print()
-#         self.events = dates
-#         return self.events
+         for day in dates:
+             print(day['date'], day['event'], sep="\n")
+             print()
+         self.events = dates
+         return self.events
 
 #     def today_events(self):
