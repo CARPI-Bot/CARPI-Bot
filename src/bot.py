00a980726d11f551e6e46d62f32c93100a5c2013
SameriteRL <raymondchen733@gmail.com>
2023-04-25 17:34:56 -0400

    Moved cogs into src, updated on_ready

--- src/bot.py
+++ src/bot.py
@@ -3,6 +3,7 @@ import discord
 from discord.ext import commands
 from globals import *
 
+# Must take in a directory (folder), path cannot be a simple file
 async def load_cogs(path:str, bot:commands.Bot):
     for file_name in os.listdir(path):
         new_path = os.path.join(path, file_name)
@@ -11,6 +12,7 @@ async def load_cogs(path:str, bot:commands.Bot):
         else:
             if file_name.endswith(".py"):
                 cog = new_path[2:-3].replace("\\", ".")
+                bot.loaded_cogs.append(cog)
                 try: await bot.load_extension(cog)
                 except: pass
 
@@ -19,9 +21,10 @@ class Bot(commands.Bot):
     def __init__(self, prefix:str, intents:discord.Intents):
         super().__init__(command_prefix=prefix, intents=intents)
         self.token = open("TOKEN.txt").read().strip()
+        self.loaded_cogs = []
 
     async def setup_hook(self):
-        await load_cogs("./cogs", self)
+        await load_cogs(".\src\cogs", self)
     
     async def on_ready(self):
         num_synced = len(await self.tree.sync())
@@ -30,7 +33,9 @@ class Bot(commands.Bot):
         print(">>" + f"Discord.py version {discord.__version__}".center(30) + "<<")
         print(f"{num_synced} command(s) synced!".center(34))
         print("==================================\n")
+        print(f"{len(self.loaded_cogs)} file(s) successfully loaded:")
+        for cog in self.loaded_cogs: print(f"- {cog}")
+        print("\n==================================\n")
         print(f"Currently deployed in {len(self.guilds)} guild(s):")
-        for guild in self.guilds:
-            print(" - " + guild.name)
+        for guild in self.guilds: print(f" - {guild.name}")
         print("\n==================================")
