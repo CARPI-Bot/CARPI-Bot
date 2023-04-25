ef99506dc03ee7c86b91a5ee3b9d633b4cc29923
miranda-zheng <miranda.mzheng@gmail.com>
2023-04-25 15:23:31 -0400

    Academic Calendar implementation
    Started implementation of the the academic calendar by using apscheduler and datetime. The Calendar webscrapes the RPI website, adding all the information into a list of dictionaries and then schedules all events according to the date.

---
+++ cogs/calendar/calendar_main.py
@@ -0,0 +1,50 @@
+import asyncio
+# from globals import *
+from time import sleep
+from datetime import date, datetime
+
+from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler import events
+from apscheduler.events import EVENT_JOB_ERROR, EVENT_JOB_MISSED, JobExecutionEvent
+from apscheduler.triggers.date import DateTrigger
+
+from discord.ext import commands
+from calendar.academic_cal import events_from_webpage
+
+schedule = BackgroundScheduler()
+
+async def send_to_channel(channel_id: int, event: str):
+
+    print("txt")
+
+# async def read_dates(dates: list):
+#     channel_id = 123456
+
+#     for date in dates:
+#         schedule.add_job(send_to_channel, 'date', run_date = date['date'],
+#                         kwargs={ "channel_id" : channel_id, "event" : date['event']})
+#         schedule.start()
+
+#     return
+
+class AcadCal(commands.Cog) :
+    def __init__(self, bot):
+        self.bot = bot
+        self.events = []
+    
+    async def send_to_channel(channel_id: int, event: str) -> None:
+        print("txt")
+
+    async def read_dates(dates: list):
+        channel_id = 123456
+
+        for date in dates:
+            schedule.add_job(send_to_channel, 'date', run_date = date['date'],
+                            kwargs={ "channel_id" : channel_id, "event" : date['event']})
+            schedule.start()
+
+        return
+
+
+    async def setup(bot):
+     await bot.add_cog(AcadCal(bot))
