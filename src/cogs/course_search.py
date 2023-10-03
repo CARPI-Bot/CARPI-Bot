bbd64310fb03a72610d3928eee15a484c3e22e11
booisnotdope <ryanisi128@gmail.com>
2023-10-03 17:42:45 -0400

    Printed a list of links to other courses.
    Started to print a list of links as a string into the console. The strings can now be manipulated later into urls which can be webscrapped again.

--- src/cogs/course_search.py
+++ src/cogs/course_search.py
@@ -5,6 +5,7 @@ import asyncio
 # for webscraping
 import requests
 from bs4 import BeautifulSoup
+import re
 #checks if each character in str is a letter, num, or a space
 def isnumalpha(c:str) -> bool:
         if (ord(c) >= 97 and ord(c) <= 122) or (ord(c) >= 48 and ord(c) <= 57 ) or ord(c) == 32:
@@ -23,25 +24,25 @@ class course_search(commands.Cog):
                 &filter%5Bkeyword%5D={arg}&filter%5Bexact_match%5D=1"
         Headers = ({'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 \
                 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36', 'Accept-Language': 'en-US, en;q=0.5'})
+
         # get html content from the website
         r = requests.get(url, headers = Headers)
         soup = BeautifulSoup(r.content, 'html.parser')
-        # all_a = soup.find_all('a')
-        all_a = soup.find_all(['td', 'a'], class_ = "td_dark", attrs={'href'})
-        # all_a = soup.find_all('a', 'strong')
-        # print(all_a)
-        # preview_course = ''
-        for a in all_a:
-            print(a)
+        links = soup('a', href=re.compile("preview_course"))
+
+        #a list of links as strings to manipulate later
+        links_str = []
+        for link in links:
+            links_str.append(link['href'])
+        print('\n')
         # print(preview_course)
         # sorting_type = soup.find('input', attrs = {'id' : "sorting_type"}).find('href')
         # print(sorting_type)
         await ctx.send("Search results for [{}]({})".format(arg, r.url))
-        await ctx.send(soup.title.text)
 
     @course_search.error
     async def course_search_error(self, ctx, error):
-        await ctx.send(str(error))
+        print(ctx.send(str(error)))
     
 async def setup(bot):
     await bot.add_cog(course_search(bot))
