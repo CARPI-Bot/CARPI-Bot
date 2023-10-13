a11d6c69fa76da55752c10da6a2307bdf2074bb1
SameriteRL <raymondchen733@gmail.com>
2023-10-13 17:23:48 -0400

    Moved assets folder to under src

--- src/globals.py
+++ src/globals.py
@@ -59,10 +59,10 @@ def readTextFile(rel_path:str) -> str:
     return content
 
 # Text file containing the token to your bot client
-token_rel_path = "./cogs/assets/TOKEN.txt"
+token_rel_path = "./assets/TOKEN.txt"
 
 # Text file containing (preferably) a single command prefix character
-cmd_prefix_rel_path = "./cogs/assets/PREFIX.txt"
+cmd_prefix_rel_path = "./assets/PREFIX.txt"
 
 # Directory containing discord.py cogs
 cogs_dir_rel_path = "./cogs"
