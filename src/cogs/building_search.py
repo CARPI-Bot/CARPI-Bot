a4c9d7f6c9d48618b898e660777001f9055b8c3a
florencewangg <143844238+florencewangg@users.noreply.github.com>
2023-10-20 16:54:38 -0400

    building_search that outputs building info
    Scrapped RPI building information/maps website and obtained the description of each building if the website provides it and the lat and long of each building. Using the lat and long and google maps link, the program takes it and inserts it into google maps link and returns the link for the directions to each building. Using the description and directions to each building, it compares the arg the user enters on discord and only outputs the information for that one building requested or multiple that have the same word in it if the full building name isn't entered.

---
+++ src/cogs/building_search.py
@@ -0,0 +1,106 @@
+import discord
+from discord.ext import commands
+import asyncio
+
+#for webscraping
+import requests
+from bs4 import BeautifulSoup
+
+class building_search(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    async def get_description(self, building_name, soup):
+        info = soup.find_all("div", {"class": "field-content"})
+        for building_info in info:
+            curr_building_name = building_info.find("h2").text
+            if curr_building_name.lower() in (building_name).lower():
+                blurbs = building_info.find("div", {"class": "grid-6"})
+                #check if blurbs is empty
+                if blurbs is not None:
+                    #find all paragraph
+                    paragraphs = blurbs.find_all('p')
+                    blurb = ""
+                    for i, paragraph in enumerate(paragraphs):
+                        #check if paragraph is empty
+                        if paragraph.text.strip():
+                            blurb += paragraph.text
+                            if i < len(paragraphs) - 1:
+                                blurb += '\n\n'
+                    return blurb
+        return 'No description'
+        
+
+    @commands.command (description = 'Searches for buildings in buildings list')
+    async def building_search(self, ctx, *, arg):
+        URL = "https://info.rpi.edu/map"
+        Headers = ({'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 \
+                (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36', 'Accept-Language': 'en-US, en;q=0.5'})
+        page = requests.get(URL, headers = Headers)
+        soup = BeautifulSoup(page.content, 'html.parser')
+
+        # info = soup.find_all("div", {"class": "field-content"})
+        # for building_info in info:
+        #     building_name = building_info.find("h2").text
+        #     if arg.lower() in (building_name).lower():
+        #         blurb = building_info.find("div", {"class": "grid-6"})
+        #         print("BLURB")
+        #         print(blurb.text)
+
+        #long lat building
+        buildings = soup.find_all("a", {"class": "rpi-poi"})
+        for building in buildings:
+            building_name = building.text
+            if arg.lower() in (building.text).lower():
+                lat = building["data-lat"]
+                long = building["data-lng"]
+                description = await self.get_description(building_name, soup)
+                description = description.lstrip()
+                # description = description.rstrip(' ')
+                # description = "N/A"
+                google_map_link = f"https://www.google.com/maps/search/?api=1&query={lat}%2C{long}"
+                await ctx.send(f"### {building_name}\n[Click for directions to {arg.title()}](<{google_map_link}>)\n```{description}```")
+        
+
+        # URL = "https://info.rpi.edu/map/Academic%20Research"
+        # Headers = ({'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 \
+        #         (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36', 'Accept-Language': 'en-US, en;q=0.5'})
+        # page = requests.get(URL, headers = Headers)
+        # soup = BeautifulSoup(page.content, 'html.parser')
+        
+        # #links = soup('a') #use this to get access to the lat and long
+        # #links = soup('span')
+
+
+        # found = False
+        # buildingName = soup.find_all('div', class_='views-field views-field-body')
+        # #building_name = soup.find('h2').text
+        
+        # for building in buildingName:
+            
+        #     #contains the 
+        #     justName = building.find('h2')
+            
+            
+       
+        #     if(arg == justName.text):
+        #         info = ("Your requested building: " + building.text)
+        #         found = True
+        # if(found):
+        #     await ctx.send(info)
+        # else:
+        #     await ctx.send("The building you have requested does not exist. Please enter another building name.")
+                
+        
+
+    @building_search.error
+    async def building_search_error(self, ctx, error):
+        await ctx.send(str(error))
+
+
+async def setup(bot):
+    await bot.add_cog(building_search(bot))
+    
+
+
+
