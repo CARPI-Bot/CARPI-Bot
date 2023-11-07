a048813057d68fa254d048bf3f9d2ae503029f62
SameriteRL <raymondchen733@gmail.com>
2023-11-07 17:20:28 -0500

    Course scraper now (attempts) to parse misc info
    Expanded get_course_info() to parse more than just the course description, which includes info like prereqs/coreqs, credit hours, etc. However, misc info is sometimes included in the description and sometimes there is no description, which a fix is actively being worked on for.
    
    Co-Authored-By: booisnotdope <143844132+booisnotdope@users.noreply.github.com>

--- src/course_scraper.py
+++ src/course_scraper.py
@@ -7,15 +7,12 @@ import time
 
 ClientSession = aiohttp.ClientSession
 
-async def get_course_info(session:ClientSession, url:str, parser:str, headers:dict, data:dict) -> None:
+async def get_course_info(session:ClientSession, url:str, parser:str, data:dict) -> None:
     info = dict()
     status_code = 0
     # Repeatedly requests the course webpage until the response is successful
     while (status_code != 200):
-        course_response = await session.get(
-            url = url,
-            headers = headers
-        )
+        course_response = await session.get(url=url)
         status_code = course_response.status
     course_soup = BeautifulSoup(await course_response.text(), parser)
     # Gets the header containing the course's name
@@ -23,17 +20,27 @@ async def get_course_info(session:ClientSession, url:str, parser:str, headers:di
     info["title"] = header.string
     # Start searching for the course description following the course title header
     elements = list(header.next_siblings)
-    course_desc = ""
-    # Gathers all text between the first <hr> and <br> tag after the header
+    course_desc, misc_info = "", ""
+    # Gathers all text between the first and second <hr> tag after the header
     for i in range(len(elements)):
         if elements[i].name == "hr":
-            for j in range(i + 1, len(elements)):
-                if elements[j].name == "br":
-                    break
-                text = elements[j].string
+            j = i + 1
+            # Between the first <hr> and <br>, read all text as the course description
+            while elements[j].name != "hr":
+                text = elements[j].get_text()
                 course_desc += text.strip() if text != None else ""
+                # Between the first <br> and second <hr>, read all text as misc info
+                if elements[j].name == "br":
+                    k = j + 1
+                    while elements[k].name != "hr":
+                        text = elements[k].get_text()
+                        misc_info += text.strip() if text != None else ""
+                        k += 1
+                    break
+                j += 1
             break
     info["description"] = course_desc
+    info["misc"] = misc_info
     data[info["title"]] = info
 
 async def courses_to_json(parser:str, domain:str, href:str, req_params:str, headers:str) -> str:
@@ -41,17 +48,16 @@ async def courses_to_json(parser:str, domain:str, href:str, req_params:str, head
     json_data = dict()
     page_num = 1
     url = f"{domain}{href}"
-    async with ClientSession() as session:
+    async with ClientSession(headers=headers) as session:
         while (url != None):
             page_start_time = time.time()
             search_response = await session.get(
                 url = url,
-                params = req_params,
-                headers = headers
+                params = req_params
             )
             # Retries the request if the response fails
             if search_response.status != 200:
-                # print(f"Bad response from page {page_num}, retrying...")
+                print(f"Bad response from page {page_num}, retrying...")
                 continue
             search_soup = BeautifulSoup(await search_response.text(), parser)
             # Finds all anchor elements that links to a course info preview
@@ -71,7 +77,6 @@ async def courses_to_json(parser:str, domain:str, href:str, req_params:str, head
                             session = session,
                             url = f"{domain}{anchor['href']}",
                             parser = parser,
-                            headers = headers,
                             data = json_data
                         )
                     )
@@ -87,6 +92,8 @@ async def courses_to_json(parser:str, domain:str, href:str, req_params:str, head
             print(f"Page {page_num} parse time: {time.time() - page_start_time:.2f}s")
             page_num += 1
     print(f"Total time to parse all courses: {time.time() - start_time:.2f}s")
+    global write_start_time
+    write_start_time = time.time()
     json_obj = json.dumps(json_data, indent = 4)
     return json_obj
 
@@ -111,6 +118,7 @@ async def main() -> None:
     )
     with open("data.json", "w") as outfile:
         outfile.write(json_obj)
+    print(f"Total time to write to JSON: {time.time() - write_start_time:.2f}s")
 
 if __name__ == "__main__":
     asyncio.run(main())
