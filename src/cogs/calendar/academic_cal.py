be668a7f4ec1c9b3825e5acfbb48eaab88df1bac
Miranda Zheng <123515762+mirmirmirr@users.noreply.github.com>
2023-12-01 15:21:10 -0500

    added comments

--- src/cogs/calendar/academic_cal.py
+++ src/cogs/calendar/academic_cal.py
@@ -1,150 +1,150 @@
+# Importing necessary modules and packages
 import requests
 from bs4 import BeautifulSoup
-from datetime import date, datetime, timedelta, timezone
-from time import sleep
+from datetime import datetime
 
+# Importing a third-party library for converting tables to ASCII art
 from table2ascii import table2ascii, PresetStyle
 import calendar
 
-# from PIL import Image, ImageDraw
-
-def events_from_webpage() :
+# Function to scrape events from the academic calendar webpage
+def events_from_webpage():
     '''
-    This funtion is the main webscrapping function of the academic calendar. It when running, it is 
-    directed to RPI's calendar page, parses through the page source of the website and organizes all 
-    the data into a dictionary for easier access. It will return a dictionary of all the events
-    listed on the calander for that particular schoolyear.
+    This function performs web scraping on RPI's academic calendar webpage. It retrieves
+    and organizes event data into a dictionary, providing a convenient format for
+    accessing information about events for a particular school year.
 
-    Something that could be improved about this function would be to make it update and webscrape
-    the website automatically instead of needing to manually run the funtion every school year.
+    Note: An improvement could be made to automate the update and scraping process,
+    eliminating the need to manually run the function for each school year.
     '''
 
-    ## accessing the website
+    # Accessing the website
     URL = "https://info.rpi.edu/registrar/academic-calendar"
     header = {
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36',
     }
 
-    ## getting the HTML content from the webpage
-    r = requests.get(URL, headers = header)
+    # Getting HTML content from the webpage
+    r = requests.get(URL, headers=header)
     soup = BeautifulSoup(r.content, 'html5lib')
 
-    ## finding the div container storing all the calender dates
-    academicCal = soup.find('div', attrs = {'id' : "academicCalendar"})
+    # Finding the div container storing all the calendar dates
+    academicCal = soup.find('div', attrs={'id': "academicCalendar"})
 
-    ## storing calendar dates into a list of dictionaries with keys for the
-    ## month, day, event name, and a url of to more details about the event.
+    # Storing calendar dates into a dictionary
     dates = {}
-    monthConversion = { "January": 1, "February": 2, "March": 3, "April": 4, "May": 5, "June": 6,
-            "July": 6, "August": 8, "September": 9, "October": 10, "November": 11, "December": 12}
-
+    monthConversion = {"January": 1, "February": 2, "March": 3, "April": 4, "May": 5, "June": 6,
+                       "July": 6, "August": 8, "September": 9, "October": 10, "November": 11, "December": 12}
 
     for month in academicCal.find_all('table'):
-        rows = month.find_all('tr')  
+        rows = month.find_all('tr')
         month = rows[0].text
         month = month.split()
         month = monthConversion[month[0]]
 
         months = []
-        for d in range(len(rows)-1):
-            day = rows[d+1].find_all('td')[0].text
+        for d in range(len(rows) - 1):
+            day = rows[d + 1].find_all('td')[0].text
 
             date = {}
             date['month'] = month
             date['date'] = day
-            date['event'] = rows[d+1].find_all('td')[1].text
-            date['url'] = rows[d+1].a['href']
+            date['event'] = rows[d + 1].find_all('td')[1].text
+            date['url'] = rows[d + 1].a['href']
             months.append(date)
         dates[month] = months
 
     return dates
 
+# Function to get the current month's calendar and events
 def getMonthAndDate(dates):
+    '''
+    This function generates a formatted ASCII calendar for the current month and returns
+    a list containing the formatted calendar and a list of events for the current month.
+    '''
 
     month = datetime.now().month
     year = datetime.now().year
-    month_name = calendar.month_name[month]    
+    month_name = calendar.month_name[month]
     cal = calendar.Calendar(calendar.SUNDAY)
-    currMonth = cal.monthdayscalendar(year,month)
+    currMonth = cal.monthdayscalendar(year, month)
     currMonth = [[day if day != 0 else "" for day in week] for week in currMonth]
     currEvents = dates[month]
 
     thin = table2ascii(
         header=["sun", "mon", "tue", "wed", "thu", "fri", "sat"],
-        body= currMonth,
+        body=currMonth,
         style=PresetStyle.thin_compact_rounded,
     )
 
-    thin = month_name+" Calendar\n"+thin
+    thin = month_name + " Calendar\n" + thin
 
     return [thin, currEvents]
 
+# Function to find events based on a prompt
 def findEvent(prompt, dates):
-    ''' dates will be the values for each month (so a list of dictionaries for events of that month) '''
-    prompt = prompt.lower()
+    '''
+    This function takes a prompt, searches for direct and other relevant matches in the
+    events, and returns a list of two dictionaries holding direct matches and other
+    relevant matches from the calendar for the remaining events of the semester.
+    '''
 
+    # Cleaning the prompt to make it easier to search through events
+    prompt = prompt.lower()
     split_prompt = prompt.split()
 
-    ## loop through the list
+    # Loop through the list of events
     fullMatches = []
-    otherMatches =[]
+    otherMatches = []
 
-    for month in dates:        
+    # Search for matches and filter through them
+    for month in dates:
         for event in month:
-            found = False
-            description = event['event']
-            description = description.lower()
+            description = event['event'].lower()
 
+            # Check for direct matches
             if description.find(prompt) != -1:
                 fullMatches.append(event)
-            else: 
+            else:
+                # Check for matches with individual words in the prompt
                 for word in split_prompt:
-                    if description.find(word+" ") != -1:
+                    if description.find(word + " ") != -1:
                         otherMatches.append(event)
                         break
     return [fullMatches, otherMatches]
 
-# def convertToImage(dates):
-#     calendar = getMonthAndDate(dates)
-#     text = calendar[0]
-
-#     width, height = (1000, 1000)
-#     im = Image.new("RGBA", (width, height), "white")
-
-#     draw = ImageDraw.Draw(im)
-#     # w, h = draw.multiline_text(text)
-
-#     draw.multiline_text(((width)/2,(height)/2), text, fill="black")
-
-#     im.save("final.png", "PNG")
-
-#     return im
-
+# Function to format the found events
 def formatFind(findList):
+    '''
+    This function takes a list of found events, separates them into direct matches and
+    other relevant matches, and returns a formatted list for each category.
+    '''
+
     fullMatches = findList[0]
-    otherMaches = findList[1]
+    otherMatches = findList[1]
 
     full = ""
     other = ""
 
+    # Format direct matches
     for event in fullMatches:
         full += "**" + event["date"] + "**\n[" + event["event"] + "](" + event["url"] + ")"
         full += "\n"
 
-    event = fullMatches[0]
-    
-    for event in otherMaches:
+    # Format other relevant matches
+    for event in otherMatches:
         other += "**" + event["date"] + "**\n[" + event["event"] + "](" + event["url"] + ")"
         other += "\n"
-    
+
     return [full, other]
 
-if __name__ == "__main__" :
+# Main block of code to test the functions
+if __name__ == "__main__":
+    # Example usage of the functions
     channel_id = 1099112664724152490
     dates = events_from_webpage()
     cal = getMonthAndDate(dates)
 
+    # Find and format events related to "no classes"
     find = findEvent("no classes", dates.values())
     find = formatFind(find)
-
-    # convertToImage(dates)
--- src/cogs/calendar/calendar_main.py
+++ src/cogs/calendar/calendar_main.py
@@ -1,122 +1,133 @@
-import asyncio
+# Importing necessary modules and packages
 from globals import *
-from time import sleep
-from datetime import date, datetime
-
 import discord
 from discord.ext import commands
 from cogs.calendar.academic_cal import events_from_webpage, getMonthAndDate, findEvent, formatFind
 import calendar
-from datetime import date, datetime, timedelta, timezone
+from datetime import datetime
 
-class AcadCal(commands.Cog) :
+# Defining a Discord Cog for Academic Calendar-related commands
+class AcadCal(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
     
+    # Command to print the current month's calendar and events
     @commands.command(description="Prints the current month's calendar and events.",
-                             aliases=["cal"])
+                      aliases=["month", "cal"])
     async def print(self, ctx):
-        
+        # Fetching events data from a webpage
         dates = events_from_webpage()
         acadCalendar = getMonthAndDate(dates)
 
-        # Create an emded for the message
+        # Creating an embed for the message
         embedVar = discord.Embed(
             title=f"Upcoming Events for {calendar.month_name[datetime.now().month]}",
             description=f"```\n{acadCalendar[0]}\n```",
         )
-
+        
+        # Adding the events for the current month to the embed
         for event in acadCalendar[1]:
             embedVar.add_field(
-                name = event['date'],
-                value = event['event'],
+                name=event['date'],
+                value=event['event'],
                 inline=False
             )
 
+        # Sending the embed as a message
         await ctx.send(embed=embedVar)
     
+    # Handling errors for the print command
     @print.error
     async def print_calendar_error(self, ctx, error):
         print(error)
     
-    @commands.command(description="Finds relavant events.",  aliases=["find"])
+    # Command to find exact matches for a given prompt
+    @commands.command(description="Finds relevant events.", aliases=["find"])
     async def findExact(self, ctx:Context, *, prompt:str):
-
+        # Fetching events data from a webpage
         dates = events_from_webpage()
         eventsList = findEvent(prompt, dates.values())
-        finds = formatFind(eventsList)
 
+        # Creating an embed for the message
         embedVar = discord.Embed(
             title=f'Showing matches for "' + prompt + '"'
         )
-
+        
+        # Adding matched events to the embed
         for event in eventsList[0]:
             embedVar.add_field(
-                name = event['date'],
-                value = "[" + event['event'] + "](" + event['url'] + ")",
-                inline = False
+                name=event['date'],
+                value="[" + event['event'] + "](" + event['url'] + ")",
+                inline=False
             )
         
+        # If there are no matches, adding a message to the embed
         if len(eventsList[0]) == 0:
             embedVar.add_field(
-                name = "",
-                value = "Sorry! There are no events :(",
-                inline = False
+                name="",
+                value="Sorry! There are no events :(",
+                inline=False
             )
         
-        # add a button to ask if the user also wants a list of relevant events
+        # Adding a button to ask if the user also wants a list of relevant events
         view = discord.ui.View()
         button = discord.ui.Button(
-                label="Find Relevant", 
-                style=discord.ButtonStyle.blurple,
-                custom_id="find_relevant_button"
+            label="Find Other Relevant Events", 
+            style=discord.ButtonStyle.blurple,
+            custom_id="find_relevant_button"
         )
-
         view.add_item(button)
 
+        # Sending the embed with the button
         message = await ctx.send(embed=embedVar, view=view)
 
+        # Handling button click event
         async def button_clicked(interaction):
             await self.findRelevant(ctx, prompt=prompt)
             await interaction.response.send_message(content="")
-        
         button.callback = button_clicked
 
+    # Handling errors for the findExact command
     @findExact.error
     async def print_calendar_error(self, ctx, error):
         print(error)
 
+    # Command to find other relevant events based on a prompt
     @commands.command(description="Finds relevant events.", aliases=["findOthers"])
     async def findRelevant(self, ctx:Context, *, prompt:str):
-
+        # Fetching events data from a webpage
         dates = events_from_webpage()
         eventsList = findEvent(prompt, dates.values())
-        finds = formatFind(eventsList)
 
+        # Creating an embed for the message
         embedVar = discord.Embed(
             title=f'Showing Related Matches for "' + prompt + '"'
         )
         
+        # Adding related events to the embed
         for event in eventsList[1]:
             embedVar.add_field(
-                name = event['date'],
-                value = event['event'],
-                inline = False
+                name=event['date'],
+                value=event['event'],
+                inline=False
             )
 
+        # If there are no matches, adding a message to the embed
         if len(eventsList[1]) == 0:
             embedVar.add_field(
-                name = "",
-                value = "Sorry! There are no more relevant events :(",
-                inline = False
+                name="",
+                value="Sorry! There are no more relevant events :(",
+                inline=False
             )
 
+        # Sending the embed as a message
         await ctx.send(embed=embedVar)
     
+    # Handling errors for the findRelevant command
     @findRelevant.error
     async def print_calendar_error(self, ctx, error):
         print(error)
 
-
+# Asynchronous function to set up the Academic Calendar Cog
 async def setup(bot):
     await bot.add_cog(AcadCal(bot))
