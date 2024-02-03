8a3ded71c90e753f4a2be1fb5699766634fb851a
jzgom067 <jzgombic3@gmail.com>
2024-02-03 06:27:43 -0500

    Add embeds to course search
    Course search results are now neatly formatted within embeds, including all information for each course from the current data set.
    When no matches are found, a different embed is displayed, with a randomly chosen tip message.

--- src/cogs/course_search.py
+++ src/cogs/course_search.py
@@ -2,6 +2,7 @@ import discord
 from discord.ext import commands
 import mysql.connector as mysql
 import re
+import random
 from globals import ERROR_TITLE, COMMAND_PREFIX, SQL_LOGIN, SQL_SCHEMA, sendUnknownError
 
 Context = commands.Context
@@ -15,6 +16,13 @@ def sanitize_str(input:str) -> str:
             [c.lower() for c in input.strip() if c.isnumeric() or c.isalpha() or c == " "]
         )
     )
+
+def get_random_tip(self) -> str:
+        rand_tip = random.randint(0, self.tips[-1][1])
+        for tip in self.tips:
+            if rand_tip <= tip[1]:
+                return tip[0]
+        return "There is no tip. Have a nice day."
 
 class CourseSearch(commands.Cog):
     def __init__(self, bot:commands.Bot):
@@ -46,8 +54,15 @@ class CourseSearch(commands.Cog):
                 OR code_match > 0
             ORDER BY
                 `name` ASC, code_match DESC, title_match DESC, title_similar DESC
-            LIMIT 20
+            LIMIT 6
         """
+        self.tips = [ # each tip has a probability threshold to appear
+            ["You can search by acronyms! Try `RCOS` or `FOCS`.", 100],
+            ["You can search by abbreviations! Try `Comp Org` or `P Soft`.", 200],
+            ["This bot was made in RCOS! Consider joining the class next semester.", 300],
+            ["You can die from radiation poisioning by eating 10 million bananas. Try not to.", 325],
+            ["We made this [video guide](https://www.youtube.com/watch?v=QX43QTYyV-8) to help new users with this bot!", 350] # the last tip has the max value for randint to generate
+        ]
     
     def __del__(self):
         self.cursor.close()
@@ -59,7 +74,8 @@ class CourseSearch(commands.Cog):
         if len(search_term) == 0:
             raise commands.MissingRequiredArgument
         reg_start_or_space = "(^|.* )"
-        regex1 = f".*{search_term}.*" # just the search itself
+        regex1 = search_term # just the search itself
+        
         regex2 = "("
         # for acronyms
         if len(search_term) > 1:
@@ -70,6 +86,7 @@ class CourseSearch(commands.Cog):
                     regex2 += f"{char}.* "
             regex2 = regex2[:-3]
         tokens = search_term.split()
+        
         # for abbreviations
         if len(tokens) > 1:
             regex2 += f"|{reg_start_or_space}"
@@ -77,20 +94,53 @@ class CourseSearch(commands.Cog):
                 regex2 += f"{token}.* "
             regex2 = regex2[:-3]
         regex2 += ")"
+        
+        self.cursor.reset()
         self.cursor.execute(self.search_query, (search_term, regex1, regex2))
-        return_string = ""
+        
+        # default embed is the "no courses found", it will only be changed if there is a match
+        return_embed = discord.Embed(
+            color = 0xff0000,
+            title = "No Matches",
+            description = "Try searching for something else."
+        )
+        return_embed.add_field(
+            name = "Tip:",
+            value = get_random_tip(self)
+        )
+
+        field_names = ["", "", "", "", "Offered", "", "Credits", "Prereq/Coreq", "Colisted", "Crosslisted", "Contact/Lecture/Lab Hours"]
+
+        related_courses = []
+
         for row_num, row in enumerate(self.cursor):
-            if (row_num == 0):
-                return_string += f"{row[0]} {row[1]}: {row[2]}\n{row[3]}\n{row[4]}\n{row[5]}\nCredits: {row[6]}\n{row[7]}\n{row[8]}\n{row[9]}\n{row[10]}\n"
-            elif (row_num == 1):
-                return_string += "Other results: \n"
-                return_string += f"{row[0]} {row[1]}: {row[2]}\n"
+            if row_num == 0:
+                return_embed = discord.Embed(
+                    color = 0xff00ff,
+                    title = f"{row[0]} {row[1]}: {row[2]}",
+                    description = row[3],
+                    url = row[5]
+                )
+                for element_num, element in enumerate(row):
+                    if element_num >= len(field_names):
+                        break
+                    if element and len(field_names[element_num]) > 0:
+                        return_embed.add_field(
+                            name = field_names[element_num],
+                            value = element,
+                            inline = False
+                        )
             else:
-                return_string += f"{row[0]} {row[1]}: {row[2]}\n"
-            # dept and code match: db[11] == 1:
-            # full title match: db[12] == 1:
-            # partial title match: db[13] == 1:
-        await ctx.send(return_string.strip("\n"))
+                related_courses.append(f"{row[0]} {row[1]}: {row[2]}\n")
+        if len(related_courses) > 0:
+            rel_cor_str = ""
+            for course in related_courses:
+                rel_cor_str += course
+            return_embed.add_field(
+                name = "Related Courses",
+                value = rel_cor_str.strip('\n')
+            )
+        await ctx.send(embed = return_embed)
 
     @course.error
     async def course_search_error(self, ctx:Context, error):
