24223f265b211f11aa571696584084a1988ac239
jzgom067 <jzgombic3@gmail.com>
2024-03-30 03:56:30 -0400

    Restore embed and add dropdown functionality
    After the restructuring of the database, the course search works again. The search algorithm was improved to prioritize more relevant results, though it has some bugs. Dropdown functionality was also implemented, but does nothing as of right now.

--- src/cogs/course_search/course_data.sql
+++ src/cogs/course_search/course_data.sql
@@ -1,15 +1,17 @@
 SELECT
-    courses.dept,
-    courses.code_num,
-    courses.title,
-    courses.desc_text,
+    courses.dept AS dept,
+    courses.code_num AS code_num,
+    courses.title AS title,
+    courses.desc_text AS desc_text,
     MIN(sections.credit_min) AS credit_min,
     MAX(sections.credit_max) AS credit_max,
     NULL AS prereqs,
     NULL AS coreqs,
     NULL AS crosslist,
     NULL AS restrictions,
-    REGEXP_LIKE(CONCAT(courses.dept, " ", courses.code_num), %s, 'i') AS code_match,
+    REGEXP_LIKE(CONCAT(courses.dept, ' ', courses.code_num), %s, 'i') AS code_match,
+    REGEXP_LIKE(courses.title, %s, 'i') AS title_exact_match,
+    REGEXP_LIKE(courses.title, %s, 'i') AS title_start_match,
     REGEXP_LIKE(courses.title, %s, 'i') AS title_match,
     REGEXP_LIKE(courses.title, %s, 'i') AS title_similar
 FROM
@@ -17,11 +19,13 @@ FROM
     LEFT JOIN sections ON courses.dept = sections.dept AND courses.code_num = sections.code_num
     LEFT JOIN prerequisites ON sections.crn = prerequisites.crn
 GROUP BY
-    1, 2, 3, 4, 7, 8, 9, 10
+    1, 2
 HAVING
-    title_match > 0
+    code_match > 0
+    OR title_exact_match > 0
+    OR title_start_match > 0
+    OR title_match > 0
     OR title_similar > 0
-    OR code_match > 0
 ORDER BY
-    `name` ASC, code_match DESC, title_match DESC, title_similar DESC
+    11 DESC, 12 DESC, 13 DESC, 14 DESC, 15 DESC, 2 ASC, 3 ASC
 LIMIT 6;
--- src/cogs/course_search/course_search.py
+++ src/cogs/course_search/course_search.py
@@ -4,10 +4,11 @@ from pathlib import Path
 
 import aiomysql
 import discord
+from discord import app_commands, Interaction
+from discord.app_commands import AppCommandError
 from discord.ext import commands
-from discord.ext.commands import CommandError, Context
 
-from globals import ERROR_TITLE, SQL_LOGIN, send_generic_error
+from globals import ERROR_TITLE, send_generic_error
 
 
 def sanitize_str(input: str) -> str:
@@ -35,7 +36,8 @@ def get_random_tip() -> str:
         ("You can search by abbreviations! Try `Comp Org` or `P Soft`.", 100),
         ("This bot was made in RCOS! Consider joining the class next semester.", 100),
         ("You can die from radiation poisioning by eating 10 million bananas. Try not to.", 25),
-        ("We made this [video guide](https://www.youtube.com/watch?v=QX43QTYyV-8) to help new users with this bot!", 25)
+        ("We made this [video guide](https://www.youtube.com/watch?v=QX43QTYyV-8) to help new " + 
+            "users with this bot!", 25)
     )
     total_prob = 1
     for tip in tips:
@@ -49,138 +51,225 @@ def get_random_tip() -> str:
     # Has a 1/total chance of appearing
     return "There is no tip. Have a nice day."
 
+def get_credits_desc(min, max) -> str:
+    """
+    A helper function that converts a minimum and maximum credit value to a description.
+    """
+    if min == max:
+        return max
+    return f"Between {min} and {max}"
+
+def get_match_type(row) -> str:
+    """
+    A helper function that, given a row returned from an SQL query, identifies and returns the type
+    of match made between the search term and the given result.
+    """
+    if row['code_match']:
+        return "Code Match"
+    if row['title_exact_match']:
+        return "Exact Title Match"
+    if row['title_start_match'] or row['title_match']:
+        return "Title Match"
+    if row['title_similar']:
+        return "Acronym or Abbreviation Match"
+
 # TODO: change search to prioritize matches at the beginning of the
 # name, convert numbers to roman numerals or back, prioritize lower
 # level classes, prioritize full title match
+# TODO: add prerequisite courses to related courses if there are no
+# others, or add them to the dropdown with a (prereq) next to it
 # TODO: if you want to make the search algorithm crazy, do research
 # about subprocesses so you don't hold up the bot
 
 class CourseSearch(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
-        self.connection = None
-        self.cursor = None
-        # Move this to an external file, maybe
+        self.db_conn = None
         query_path = Path("./cogs/course_search/course_data.sql").resolve()
         with query_path.open() as query:
             self.course_query = query.read()
     
     async def cog_load(self) -> None:
-        try:
-            self.connection: aiomysql.Connection = await aiomysql.connect(**SQL_LOGIN)
-        except:
-            raise Exception(
-                "Could not connect to MySQL database. Make sure the server " \
-                + "is running, and that your login information is correct"
-            )
-        self.cursor: aiomysql.Cursor = await self.connection.cursor()
+        self.db_conn = await self.bot.sql_conn_pool.acquire()
     
     async def cog_unload(self) -> None:
-        if self.connection:
-            await self.cursor.close()
-            await self.connection.close()
+        self.db_conn.close()
 
-    @commands.hybrid_command(
+    @app_commands.command(
+        name = "course",
         description = "Search for a course in the RPI catalog!"
     )
-    async def course(self, ctx: Context, *, search_term: str):
+    async def course(self, interaction: Interaction, *, search_term: str):
         search_term = sanitize_str(search_term)
         if len(search_term) == 0:
-            raise commands.BadArgument
-        reg_start_or_space = "(^|.* )"
-        # Just the search itself
-        regex1 = search_term
-        regex2 = "("
-        # For acronyms
-        if len(search_term) > 1:
-            regex2 += reg_start_or_space
-            for char in search_term:
-                # Don't use spaces in acronyms
-                if char != " ":
-                    regex2 += f"{char}.* "
-            regex2 = regex2[:-3]
-        tokens = search_term.split()
-        
-        # For abbreviations
-        if len(tokens) > 1:
-            regex2 += f"|{reg_start_or_space}"
-            for token in tokens:
-                regex2 += f"{token}.* "
-            regex2 = regex2[:-3]
-        regex2 += ")"
-        
-        await self.cursor.execute(self.course_query, (search_term, regex1, regex2))
-        
-        # Default embed is "no matches", it will only be changed if
-        # there is a match.
-        return_embed = discord.Embed(
-            title = "No Matches",
-            description = "Try searching for something else.",
-            color = 0xC80000
+            raise "Bad argument"
+        view = CourseMenu(interaction, self.db_conn, self.course_query)
+        await view.course_query(search_term)
+        await interaction.response.send_message(
+            view = view,
+            embed = view.embed,
+            ephemeral = False
         )
-        return_embed.add_field(
-            name = "Tip:",
-            value = get_random_tip()
-        )
-
-        field_names = (
-            "",
-            "",
-            "",
-            "",
-            "Offered",
-            "",
-            "Credits",
-            "Prereq/Coreq",
-            "Colisted",
-            "Crosslisted",
-            "Contact/Lecture/Lab Hours"
-        )
-        similar_matches = ""
-        for row_num, row in enumerate(await self.cursor.fetchall()):
-            if row_num == 0:
-                return_embed = discord.Embed(
-                    title = f"{row[0]} {row[1]}: {row[2]}",
-                    description = row[3],
-                    url = row[5],
-                    color = 0xff00ff
-                )
-                for element_num, element in enumerate(row):
-                    if element_num >= len(field_names):
-                        break
-                    if element and len(field_names[element_num]) > 0:
-                        return_embed.add_field(
-                            name = field_names[element_num],
-                            value = element,
-                            inline = False
-                        )
-            else:
-                similar_matches += f"{row[0]} {row[1]}: {row[2]}\n"
-        if len(similar_matches) > 0:
-            return_embed.add_field(
-                name = "Similar Matches",
-                value = similar_matches.strip('\n')
-            )
-        await ctx.send(embed = return_embed)
 
     @course.error
-    async def course_error(self, ctx: Context, error: CommandError):
+    async def course_error(self, interaction: Interaction, error: AppCommandError):
         embed_desc = None
-        if isinstance(error, commands.CommandInvokeError):
-            error = error.original
-        if isinstance(error, commands.MissingRequiredArgument):
-            embed_desc = f"Usage: `{self.bot.command_prefix}course [search term]`"
-        elif isinstance(error, commands.BadArgument):
+        if error == "Bad argument":
             embed_desc = f"Your input is invalid! Try something else."
+        else:
+            error = error.original
         if embed_desc is not None:
             embed_var = discord.Embed(
                 title = ERROR_TITLE,
                 description = embed_desc,
                 color = 0xC80000
             )
-            await ctx.send(embed=embed_var)
+            await interaction.response.send_message(embed=embed_var)
         else:
-            await send_generic_error(ctx, error)
+            # Interaction errors not supported yet by the bot
+            print(error)
     
+class CourseMenu(discord.ui.View):
+    def __init__(
+        self,
+        interaction: Interaction,
+        db_conn: aiomysql.Connection,
+        query: str
+    ):
+        super().__init__()
+        self.interaction = interaction
+        self.db_conn = db_conn
+        self.query = query
+        self.related_courses = []
+        self.embed = discord.Embed(
+            title = "Course Search Error",
+            description = "If you're seeing this, someting went wrong.",
+            color = 0xC80000
+        )
+
+    async def course_query(self, search_term):
+        """
+        This function modifies this CourseMenu to use the specified query, and change the embed to
+        show the resulting course.
+        """
+        reg_start_or_space = "(^|.* )"
+        # Just the search itself
+        regex1 = search_term
+        # A full title match
+        regex2 = f"^{search_term}$"
+        # Title matched at the beginning
+        regex3 = f"^{search_term}"
+        regex4 = "("
+        # For acronyms
+        if len(search_term) > 1:
+            regex4 += reg_start_or_space
+            for char in search_term:
+                # Don't use spaces in acronyms
+                if char != " ":
+                    regex4 += f"{char}.* "
+            regex4 = regex4[:-3]
+        tokens = search_term.split()
+        
+        # For abbreviations
+        if len(tokens) > 1:
+            regex4 += f"|{reg_start_or_space}"
+            for token in tokens:
+                regex4 += f"{token}.* "
+            regex4 = regex4[:-3]
+        regex4 += ")"
+        
+        # Default embed is "no matches", it will only be changed if there is a match.
+        self.embed = discord.Embed(
+            title = "No Matches",
+            description = "Try searching for something else.",
+            color = 0xC80000
+        )
+        self.embed.add_field(
+            name = "Tip:",
+            value = get_random_tip()
+        )
+
+        # Execute the query
+        async with self.db_conn.cursor(aiomysql.DictCursor) as cursor:
+            await cursor.execute(
+                self.query,
+                (search_term, regex1, regex2, regex3, regex4)
+            )
+
+            # The dictionary keys, and the corresponding field names for the embed
+            field_names = (
+                ['prereqs', "Prerequisites"],
+                ['coreqs', "Corequisites"],
+                ['crosslist', "Crosslisted"],
+                ['restrictions', "Restrictions"]
+            )
+            # Get rid of UI elements, in case they are not needed for the next result
+            self.clear_items()
+            self.related_courses = []
+            rel_courses_field = ""
+            match_type = ""
+            # Fetch all data from the cursor, then parse through it
+            for row_num, row in enumerate(await cursor.fetchall()):
+                print(f"{row['dept']} {row['code_num']}: {row['title']} - {row['code_match']} {row['title_exact_match']} {row['title_start_match']} {row['title_match']} {row['title_similar']}")
+                # The first result is the main result
+                if row_num == 0:
+                    self.embed = discord.Embed(
+                        title = f"{row['dept']} {row['code_num']}: {row['title']}",
+                        color = 0xff00ff
+                    )
+                    if row['desc_text'] is not None:
+                        self.embed.description = row['desc_text']
+                    self.embed.add_field(
+                        name = "Credits",
+                        value = get_credits_desc(row['credit_min'], row['credit_max']),
+                        inline = False
+                    )
+                    for element in field_names:
+                        if row[element[0]] is not None:
+                            self.embed.add_field(
+                                name = element[1],
+                                value = row[element[0]],
+                                inline = False
+                            )
+                    match_type = get_match_type(row)
+                # Anything not the first result is just "related"
+                else:
+                    self.related_courses.append(
+                        discord.SelectOption(
+                            label = f"{row['dept']} {row['code_num']}: {row['title']}",
+                            value = f"{row['dept']} {row['code_num']}"
+                        )
+                    )
+                    rel_courses_field += f"{row['dept']} {row['code_num']}: {row['title']}\n"
+            if len(rel_courses_field) > 0:
+                self.embed.add_field(
+                    name = "Related Courses",
+                    value = rel_courses_field.strip("\n"),
+                    inline = False
+                )
+            self.embed.set_author(name = f"{search_term} → {match_type}!")
+        if len(self.related_courses) > 0:
+            dropdown = RelatedCourseDropdown(
+                placeholder = "Choose a related course to learn more...",
+                options = self.related_courses
+            )
+            self.add_item(dropdown)
+
+class RelatedCourseDropdown(discord.ui.Select):
+    def __init__(
+        self,
+        options: list[discord.SelectOption],
+        placeholder: str,
+        **kwargs
+    ):
+        super().__init__(
+            options = options,
+            placeholder = placeholder,
+            **kwargs
+        )
+
+    async def callback(self, interaction: Interaction) -> None:
+        await interaction.response.send_message("🫡")
+
 async def setup(bot: commands.Bot):
     await bot.add_cog(CourseSearch(bot))
